0cb4b8f4c52ed2db60a8f8d13ea018c511aa6064
Pavlo Karas <kar.pavel123@gmail.com>
2023-08-14 18:57:37 +0300

    Initial commit

---
+++ first_lab/first_lab/first_labApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_labApp.swift
+//  first_lab
+//
+//  Created by Павло Карась on 14.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct first_labApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
