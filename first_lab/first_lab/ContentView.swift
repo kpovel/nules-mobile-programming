9b49007229ce1c06231f74e6afed11a6195fab75
Pavlo Karas <kar.pavel123@gmail.com>
2023-08-14 19:45:34 +0300

    solved 1st lab

--- first_lab/first_lab/ContentView.swift
+++ first_lab/first_lab/ContentView.swift
@@ -1,20 +1,46 @@
-//
-//  ContentView.swift
-//  first_lab
-//
-//  Created by Павло Карась on 14.08.2023.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        VStack (alignment: .center) {
+            Text("Лабораторна робота №1")
+                .font(.title)
+                .fontWeight(.heavy)
+                .padding(.bottom, 30)
+            VStack (alignment: .leading, spacing: 30) {
+                VStack (alignment: .leading){
+                    Text("Author:")
+                        .font(.title3)
+                        .fontWeight(.bold)
+                    Text("Карась Павло Петрович")
+                        .font(.headline)
+                }
+                VStack(alignment: .leading){
+                    Text("Спеціальність:")
+                        .font(.title3)
+                        .fontWeight(.bold)
+                    Text("Інженерія програмного забезпечення")
+                        .font(.headline)
+                }
+                VStack(alignment: .leading) {
+                    Text("Номер курсу і групи:")
+                        .font(.title3)
+                        .fontWeight(.bold)
+                    Text("Інженерія програмного забезпечення")
+                        .font(.headline)
+                }
+                VStack (alignment: .leading){
+                    Text("Очікування від навчального курсу: ")
+                        .font(.title3)
+                        .fontWeight(.bold)
+                    Text("Очікую вміло навчитась розроблювати мобільні застосунки")
+                        .font(.headline)
+                }
+            }
+            Spacer()
         }
+        
         .padding()
     }
 }
